a07423ed75697dab0919aaee52287007cedae587
Matthias Viehweger <kronn@kronn.de>
2010-06-11 17:58:22 +0200

    added VERSION-constant

--- lib/to_pwd.rb
+++ lib/to_pwd.rb
@@ -1,4 +1,6 @@
 module ToPwd
+  VERSION = File.read(File.join(File.dirname(__FILE__), '../VERSION')).strip
+
   autoload :Converter,         'lib/to_pwd/converter'
   autoload :StringConversions, 'lib/to_pwd/string_conversions'
   autoload :Base,              'lib/to_pwd/base'
--- test/test_base.rb
+++ test/test_base.rb
@@ -9,4 +9,8 @@ class TestBase < Test::Unit::TestCase
       assert_equal "Ds1P@dF", pwd
     end
   end
+
+  def test_version
+    assert_equal Pathname.new("#{File.dirname(__FILE__)}/../VERSION").expand_path.read.chomp, ToPwd::VERSION
+  end
 end
