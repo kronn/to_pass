19aa8f48b5e58bd64afc91036b58050c83351240
Matthias Viehweger <kronn@kronn.de>
2010-07-22 21:50:51 +0200

    refactored converters-test

--- test/test_converters.rb
+++ test/test_converters.rb
@@ -9,21 +9,25 @@ class TestConverters < Test::Unit::TestCase
   end
 
   def test_downcase
-    assert_converter 'test', :downcase, 'tEsT'
+    assert_converter 'test', 'downcase', 'tEsT'
   end
 
   def test_remove_repetition
-    assert_converter 'helo', :remove_repetition, 'helo'
-    assert_converter 'hel2o', :remove_repetition, 'hello'
-    assert_converter 'hel3o', :remove_repetition, 'helllo'
+    assert_converter 'helo',  'remove_repetition', 'helo'
+    assert_converter 'hel2o', 'remove_repetition', 'hello'
+    assert_converter 'hel3o', 'remove_repetition', 'helllo'
   end
 
   protected
 
   def assert_converter(expected, rule, string = 'test')
     assert_nothing_raised LoadError do
-      result = ToPass::Converter.new({}).send(:apply_rule, string, rule.to_s)
-      assert_equal expected, result, "Converter '#{rule}' should convert #{string} to #{expected}."
+      result = converter.send(:apply_rule, string, rule)
+      assert_equal expected, result, "Converter '#{rule.inspect}' should convert #{string} to #{expected}."
     end
+  end
+
+  def converter
+    ToPass::Converter.new({})
   end
 end
