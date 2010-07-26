2505826560e5960382702b5bdd23a49b78942d34
Matthias Viehweger <kronn@kronn.de>
2010-07-26 20:01:42 +0200

    added tests for the bundled algorithms

---
+++ test/test_algorithms.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# vim:ft=ruby:enc=utf-8
+
+require File.dirname(__FILE__)+'/helper'
+
+class TestAlgorithms < Test::Unit::TestCase
+  def test_basic_de
+    assert_algorithm 't35t', 'test', :basic_de
+    assert_algorithm 'Ds1P@dF', 'Da steht ein Pferd auf dem Flur', :basic_de
+  end
+
+  def test_basic_en
+    assert_algorithm "t35t", 'test', :basic_en
+    assert_algorithm "ti1p4u2", 'there is one problem for us, too', :basic_en
+  end
+
+  def test_secure
+    assert_algorithm '$78bRk0N5fKt5Et', 'test', :secure
+    assert_algorithm '5P2fWb0Wf%$52Cm', 'my cat is cute', :secure
+  end
+
+  protected
+
+  def assert_algorithm(expected, input, algorithm)
+    assert_nothing_raised do
+      pwd = ToPass::Base.new(input, algorithm).to_s
+      assert_equal expected, pwd
+    end
+  end
+end
