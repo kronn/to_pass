a612d5227073ecc2428982d28bd43bb63351ee49
Matthias Viehweger <kronn@kronn.de>
2010-09-03 17:35:54 +0200

    declare the constants defensively

--- lib/to_pass/version.rb
+++ lib/to_pass/version.rb
@@ -2,19 +2,19 @@
 
 module ToPass
   # version of gem
-  VERSION = '0.6.0'
+  VERSION = '0.6.0' unless defined?(ToPass::VERSION)
 
   # name of gem
-  APP_NAME = 'to_pass'
+  APP_NAME = 'to_pass' unless defined?(ToPass::APP_NAME)
 
   # date of last modification of the version
-  DATE = File.mtime(__FILE__)
+  DATE = File.mtime(__FILE__) unless defined?(ToPass::DATE)
 
   # Purpose in one sentence
-  SUMMARY = 'generate password from words or sentences'
+  SUMMARY = 'generate password from words or sentences' unless defined?(ToPass::SUMMARY)
 
   # short description, see README for details
-  DESCRIPTION = <<-EOD
+  DESCRIPTION = <<-EOD unless defined?(ToPass::DESCRIPTION)
     Passwords should be easy to remember and hard to guess.
     One technique is to have a sentence which can be easily remembered transformed to a password.
 
