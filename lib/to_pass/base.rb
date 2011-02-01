e4289a6ed82b0703ab36e95a8f4cf385af30fe1a
Matthias Viehweger <kronn@kronn.de>
2011-02-01 21:11:32 +0100

    ToPass::Base#password returns the password
    this is some what more readable in code...

--- lib/to_pass/base.rb
+++ lib/to_pass/base.rb
@@ -5,20 +5,23 @@ module ToPass
   #
   # Given a string and a algorithm identifier, the right rules
   # are loaded and applied to the string. With a simple "to_s",
-  # you can get the final password.
+  # you can get the final password. The password is also readable
+  # directly through ToPass::Base#password.
   #
   # see ToPass::AlgorithmReader and ToPass::Converter for details
   class Base
+    attr_reader :password
+
     # transform a string according to a certain algorithm
-    def initialize( string, algorithm )
+    def initialize(string, algorithm)
       rules = AlgorithmReader.load(algorithm)
-      converter = Converter.new( rules )
+      converter = Converter.new(rules)
       @password = converter.convert(string)
     end
 
     # return the generated password
     def to_s
-      @password
+      password
     end
   end
 end
